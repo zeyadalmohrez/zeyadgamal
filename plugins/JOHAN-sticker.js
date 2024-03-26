3f38b13a57a9ce35bc9b587b419e8b76b1423a15
johan <156672332+johan-hadini@users.noreply.github.com>
2024-03-26 01:38:54 +0000

    Update JOHAN-sticker.js

---
+++ plugins/JOHAN-sticker.js
@@ -0,0 +1,12 @@
+let axios = require('axios')
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn }) => {
+    let res = await axios.get('https://telegra.ph/file/ea03bbc1d2dbd77af8a57.jpg', { responseType: 'arraybuffer' })
+    if (!res.data) throw 'Failed to fetch data'
+    let sticker = await conn.sticker(res.data, false, { packname: 'Sticker Pack', author: '@example' })
+    await conn.sendMessage(m.chat, sticker, MessageType.sticker, { quoted: m })
+}
+
+handler.command = /^(يوهان)$/i
+module.exports = handler
